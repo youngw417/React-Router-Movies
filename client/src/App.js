93a4d2affac7b69cf5039b18a8ac808e84c4573e
Young Won <youngw417@gmail.com>
2019-11-12 20:21:11 -0800

    movie list MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -9,10 +11,18 @@ const App = () => {
     setSavedList( [...savedList, movie] );
   };
 
+  
+
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      {console.log('saved list', savedList)}
+      <div>
+        <Route exact path ="/" component ={MovieList} />
+        <Route path = "/movies/:id" render = { (props) => <Movie {...props} addToSavedList = {addToSavedList} />
+           } />
+
+      </div>
     </div>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
+
+const divStyle  = {
+  display: 'flex',
+  flexDirection: 'column'
+}
+const SavedList = props => {
+
+  
+const routeToHome = () => props.history.push('/')
+
+  return(
+  <div className="saved-list" >
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+    <div style = {divStyle}>
+      {props.list.map(movie => (
+        <NavLink to= {`/movies/${movie.id}`}>
+         <span className="saved-movie">{movie.title}</span>
+        </NavLink>
+     
     ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+    </div>
+    
+    <Link to ="/">
+      <div className="home-button" onClick = { ()=> routeToHome}>Home</div>
+      </Link>
+  </div>)
+};
 
 export default SavedList;
